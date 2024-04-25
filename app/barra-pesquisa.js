3ca802409d4ab5c2904c1dfc073f1e3da0bb63b4
Felype <132705654+FelypeDantas@users.noreply.github.com>
2024-04-24 21:35:58 -0300

    barra de pesquisa arrumada

--- app/barra-pesquisa.js
+++ app/barra-pesquisa.js
@@ -4,19 +4,39 @@ barraDePesquisa.addEventListener("input", filtrarPesquisa);
 
 function filtrarPesquisa(){
     const livros = document.querySelectorAll('.livro');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
 
-    if(barraDePesquisa.value != ""){
-        for(let livro of livros){
-            let titulo = livro.querySelector('.livro__titulo').textContent.toLowerCase();
-            let valorFiltro = barraDePesquisa.value.toLowerCase();
+    livros.forEach(livro => {
+        const titulo = livro.querySelector('.livro__titulo').textContent.toLowerCase();
 
-            if(!titulo.includes(valorFiltro)){
-                livro.style.display = 'none';
-            } else{
-                livro.style.display = 'block';
-            }
+        if (!titulo.includes(valorFiltro)) {
+            livro.classList.add('hidden');
+        } else {
+            livro.classList.remove('hidden');
         }
-    } else{
-        livros.style.display = 'block';
-    }
+    });
 }
+
+// const barraDePesquisa = document.querySelector('.pesquisar__input');
+
+// barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+// function filtrarPesquisa(){
+//     const livros = document.querySelectorAll('.livro');
+
+//     if(barraDePesquisa.value != ""){
+//         for(let livro of livros){
+//             let titulo = livro.querySelector('.livro__titulo').textContent.toLowerCase();
+//             let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+//             if(!titulo.includes(valorFiltro)){
+//                 livro.style.display = 'none';
+//             } else{
+//                 livro.style.display = 'block';
+//             }
+//         }
+//     } else{
+//         livros.style.display = 'block';
+//     }
+// }
+
